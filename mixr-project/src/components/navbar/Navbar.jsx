a8d775cc6b510fe5b93a07c3db69e12d7c5ca1cd
Rithvich Ramesh <rithvichr@gmail.com>
2024-01-16 00:53:42 -0500

    added basic styling and UI components of the navbar

--- mixr-project/src/components/navbar/Navbar.jsx
+++ mixr-project/src/components/navbar/Navbar.jsx
@@ -3,7 +3,22 @@ import "./Navbar.scss"
 
 const Navbar = () => {
     return(
-        <div className='navbar'>Navbar</div>
+        <div className='navbar'>
+            <div className="container">
+                <div className="logo">
+                    <span className='text'>MIXR</span>
+                    <span className='dot'>.</span>
+                </div>
+                <div className="links">
+                    <span>Explore Musicians</span>
+                    <span>Find a Gig</span>
+                    <span>About</span>
+                    <span>Sign In</span>
+                    <button>Join</button>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
