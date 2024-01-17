95370261f7cf0769d2160f2a2efc3eb37e09a08a
Rithvich Ramesh <rithvichr@gmail.com>
2024-01-16 20:29:50 -0500

    implemented infinite slider carousel using CatCard Slider and ProjectCard componenets, added data.js

--- mixr-project/src/pages/home/home.jsx
+++ mixr-project/src/pages/home/home.jsx
@@ -1,11 +1,20 @@
 import React from "react"
 import "./home.scss"
 import Featured from "../../components/featured/Featured"
+import {cards} from "../../data"
+import Slide from "../../components/slide/Slide"
+import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 
 const Home = () => {
     return(
         <div className="home">
             <Featured/>
+            <Slide slidesToShow={5} arrowsScroll={5}>
+            {cards.map((card) => (
+                  <CatCard key={card.id} card={card} />
+            ))}
+            </Slide>
         </div>
     )
 }
